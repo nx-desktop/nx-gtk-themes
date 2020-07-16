cec7baa926b44b67edb48085fd5671926fa11db0
Luis Lavaire <llavaire01@gmail.com>
2020-07-15 22:46:24 -0600

    updated build script and Travis CI configuration file. [ci skip]

--- sources/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-find | entr -c -r -d -p -s "./lib.sh"
--- travis-build.sh
+++ travis-build.sh
@@ -1,20 +1,47 @@
-#!/bin/bash
+#! /bin/sh
+
 
 apt-get --yes update
 apt-get --yes install wget
 
-### Add KDENeon Repository
-echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main' | tee /etc/apt/sources.list.d/neon-stable.list
+
+#	add KDE Neon repository.
+
+> /etc/apt/sources.list.d/neon-stable.list \
+	echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main'
+
 wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 
-### Install Dependencies
+
+#	install dependencies.
+
 apt-get --yes update
 apt-get --yes dist-upgrade
-apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs
+apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs inkscape sassc
 mk-build-deps -i -t "apt-get --yes" -r
 
-### Build Deb
+
+#	render images & compile themes.
+
+(
+	cd src
+	for var in nomad*; do
+		mkdir -p "themes/$var/gtk-3.0/img"
+
+		cat "$src/index" | while read id; do
+			inkscape "$var/img.svg" -ji "$id" -o "themes/$var/gtk-3.0/img/$id.png"
+			inkscape "$var/img.svg" -ji "$id" -d 192 -o "themes/$var/gtk-3.0/img/$id@2.png"
+		done
+
+		sassc -t compressed "$var/scss/gtk.scss" "themes/$var/gtk-3.0/gtk.css"
+	done
+
+	mv themes ..
+)
+
+
+#	build deb.
 mkdir source
-mv ./* source/ # Hack for debuild
+mv ./* source/ # hack for debuild
 cd source
 debuild -b -uc -us
