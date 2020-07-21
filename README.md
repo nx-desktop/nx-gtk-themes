269ec4434863566e1ecd2fff7d4b522441c3b29e
Luis Lavaire <llavaire01@gmail.com>
2020-07-21 16:58:03 -0600

    update.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# NX GTK Themes [![Build Status](https://travis-ci.org/nx-desktop/nx-gtk-themes.svg?branch=master)](https://travis-ci.org/nx-desktop/nx-gtk-themes)
+# Nomad GTK Themes [![Build Status](https://travis-ci.org/nomad-desktop/nomad-gtk-themes.svg?branch=master)](https://travis-ci.org/nomad-desktop/nomad-gtk-themes)
 
-This is the repository for the NX GTK Themes.
+Minimal theme for GTK.
+
+![](https://i.imgur.com/wk9n38t.png)
+![](https://i.imgur.com/IvGnDFp.png)
 
 # Requirements
 - GTK 3.20+.
@@ -16,4 +19,4 @@ If you find problems with the contents of this repository please create an issue
 
 Artwork is Licensed under the [Creative Commons Attribution-Share Alike 4.0 License](https://creativecommons.org/licenses/by-sa/4.0/).
 
-©2019 Nitrux Latinoamericana S.C.
+©2018 Nitrux Latinoamericana S.C.
---
+++ build.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+
+apt-get --yes update
+apt-get --yes install wget
+
+
+#	add KDE Neon repository.
+
+> /etc/apt/sources.list.d/neon-stable.list \
+	echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main'
+
+wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
+
+
+#	install dependencies.
+
+apt-get --yes update
+apt-get --yes dist-upgrade
+apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs inkscape sassc
+mk-build-deps -i -t "apt-get --yes" -r
+
+
+#	render images & compile themes.
+
+(
+	cd src
+	for var in nitrux*; do
+		mkdir -p "themes/$var/gtk-3.0/img"
+
+		cat "$src/index" | while read id; do
+			inkscape "$var/img.svg" -ji "$id" -o "themes/$var/gtk-3.0/img/$id.png"
+			inkscape "$var/img.svg" -ji "$id" -d 192 -o "themes/$var/gtk-3.0/img/$id@2.png"
+		done
+
+		sassc -t compressed "$var/scss/gtk.scss" "themes/$var/gtk-3.0/gtk.css"
+		cp -r "$var/gtk-2.0" "themes/$var"
+	done
+
+	mv themes ..
+)
+
+
+#	build deb.
+mkdir source
+mv ./* source/ # hack for debuild
+cd source
+debuild -b -uc -us
